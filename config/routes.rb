a1ea76aedbdd0e7adb8e37d3de2140c515d8078b
yuta.yamamoto <1mg6040y@komazawa-u.ac.jp>
2019-11-03 10:06:14 +0000

    microposts

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+    root to: 'toppages#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :new, :create] do
+      member do
+          get :followings
+          get :followers
+      end
+    end
+    resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
